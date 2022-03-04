a069561fae3d056695a552681ea729124191a0e7
Chloe Ranahan <ChloeRanahan@cmail.carleton.ca>
2022-03-04 12:58:10 -0500

    first commit

---
+++ MNA_d.m
@@ -0,0 +1,88 @@
+clear
+
+R1 = 1;
+R2 = 2;
+L = 0.2;
+R3 = 10;
+a = 100;
+R4 = 0.1;
+Ro = 1000;
+
+G1 = 1/R1;
+G2 = 1/R2;
+G3 = 1/R3;
+G4 = 1/R4;
+Go = 1/Ro;
+
+figure(1)
+hist(0.05*randn(1000,1)+ 0.25,20)
+xlabel('C1')
+ylabel('Number')
+
+% Qd)
+
+Vin = 1;
+omega = pi;
+N = 9;
+
+%X = [V1 V2 V3 V4 Vo Iin IL I3 I4];
+n3 = 3;
+no = 5;
+
+G = zeros(N);  
+F = zeros(N,1);
+C = zeros(N);
+
+
+G(1,1) = 1;
+F(1) = Vin;
+
+G(2,1) = G1; G(2,2) = -G1; G(2,6) = 1;
+
+G(3,1) = -G1; G(3,2) = G1 + G2; G(3,7) = 1;
+
+G(4,2) = 1; G(4,3) = -1;
+C(4,7) = -L;
+
+G(5,3) = G3; G(5,8) = -1;
+
+G(6,7) = 1; G(6,8) = 1;
+
+G(7,4) = 1; G(7,8) = -a;
+
+G(8,4) = G4; G(8,5) = -G4; G(8,9) = 1;
+
+G(9,4) = -G4; G(9,5) = G4 + Go;
+
+
+V3 = [];
+Vo = [];
+
+C1 = 0.05*randn(1000,1)+ 0.25;
+
+for i = 1:1000
+    
+    C(2,1) = C1(i);
+    C(2,2) = -C1(i);
+    
+    C(3,1) = -C1(i);
+    C(3,2) = C1(i);
+
+    CFinal = C.*(1i*omega);
+    
+    H = G + CFinal;
+    V = H\F;
+    
+    V3(i) = V(n3);
+    Vo(i) = V(no);
+    
+end
+
+Gain1 = real(Vo)/Vin;
+
+figure(2)
+hist(Gain1,20)
+xlabel('Gain (Vo/Vin)')
+ylabel('Number')
+
+
